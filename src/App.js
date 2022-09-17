02168e1c17952302937f52a7483e1d4a3d5d6537
D3EVIL <shahpreet2404@gmail.com>
2022-09-17 08:33:16 -0700

    Rearragned

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from
+  "react-router-dom";
+import Home from "./components/Home";
+import About from './components/About';
+// import Resume from './components/Resume';
+// import Trash from './components/Trash';
+import News from './components/News';
+// import Notes from './components/Notes';
+// import NoteState from './context/Notes/NoteState';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <NoteState> */}
+        <Router>
+          <Navbar>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              {/* <Route path="/resume" element={<Resume />} /> */}
+              {/* <Route path="/trash" element={<Trash />} /> */}
+              <Route path="/news" element={<News />} />
+              {/* <Route path="/notes" element={<Notes />} /> */}
+            </Routes>
+          </Navbar>
+        </Router>
+      {/* </NoteState> */}
+
+    </>
   );
 }
 
